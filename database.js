473b10b7987c393139d852c14f75e179a5258247
PC Station Voranas <voranasyerdnov@gmail.com>
2023-12-29 10:17:15 +0300

    fix\

--- database.js
+++ database.js
@@ -10,22 +10,46 @@ const knex = require('knex')({
 module.exports = async () => {
     try {
         // Create a table
-        await knex.schema.createTableIfNotExists('types', (table) => {
-            table.increments('id');
+        // await knex.schema.createTableIfNotExists('menu', (t) => {
+        //     t.increments('id').primary();
+        //     t.text('data').nullable();
+        //     t.integer('parent_id').unsigned().nullable();
+        //     t.boolean('is_active').defaultTo(1);
+
+        //     t.foreign('parent_id').references('id').inTable('menu');
+        // })
+        await knex.schema.createTableIfNotExists('movies', (table) => {
+            table.increments('id').primary();
             table.string('name');
             table.boolean('has_active').defaultTo(1);
         })
-    
-        .createTableIfNotExists('season', (table) => {
-            table.increments('id');
+        .createTableIfNotExists('seasons', (table) => {
+            table.increments('id').primary();
             table.string('name');
             table.boolean('has_active').defaultTo(1);
+            table.integer('movie_id').unsigned();
+            
+            table.foreign('movie_id').references('id').inTable('movies');
         })
-        // ...and another
         .createTableIfNotExists('series', (table) => {
-            table.increments('id');
+            table.increments('id').primary();
             table.string('name');
+            table.text('telegram_file_id').nullable();
             table.boolean('has_active').defaultTo(1);
+            table.integer('season_id').unsigned();
+            
+            table.foreign('season_id').references('id').inTable('seasons');
+        })
+        .createTableIfNotExists('users', (t) => {
+            t.increments('id').primary();
+            t.bigInteger('telegram_id').nullable();
+            t.integer('select_movie_id').unsigned().nullable();
+            t.integer('select_seasons_id').unsigned().nullable();
+            t.integer('select_series_id').unsigned().nullable();
+
+           t.foreign('select_movie_id').references('id').inTable('movies');
+           t.foreign('select_seasons_id').references('id').inTable('seasons');
+           t.foreign('select_series_id').references('id').inTable('series');
         });
     
     // Finally, add a catch statement
--- main.js
+++ main.js
@@ -5,6 +5,15 @@ const TelegramBot = require('node-telegram-bot-api');
 let bot;
 let db;
 
+const commands = [
+  {
+
+      command: "start",
+      description: "Основное меню"
+
+  }
+]
+
 async function _main() {
     db = await require("./database")();
 
@@ -18,50 +27,228 @@ async function _main() {
     });
 
     bot.on("polling_error", err => console.log(err));
+  
+    bot.setMyCommands(commands);
+
+    bot.on('video', async video => {
+      //чтобы не париться с админкой, мы будем добавлять видео следующим путем
+      //каждое имя файла будет соответствовать с file_id
+      
+      if (video.from.id != process.env.ADMIN_USER_UPLOAD_ID) {
+        return;
+      }
+      
+      let name = video.video.file_name
+      let parts = name.split('.')[0].split('_');
+
+      if (parts.length < 3) {
+        await bot.sendMessage(video.chat.id, 'Не верный формат название файла который пытались загрузить (' + name + '). Он должен называться примерно так: `Любое название_Название сезона_Название серии.mp4`');
+        return;
+      }
+      
+      let movie = await db('movies').where('name', parts[0]).first();
+      
+      if (!movie) {
+        await db('movies').insert({name: parts[0]});
+        movie = await db('movies').where('name', parts[0]).first();
+      }
+      
+      let season = await db('seasons').where('movie_id', movie.id).where('name', parts[1]).first();
+      if (!season) {
+        await db('seasons').insert({name: parts[1], movie_id: movie.id});
+        season = await db('seasons').where('movie_id', movie.id).where('name', parts[1]).first();
+      }
+
+      let serie = await db('series').where('season_id', season.id).where('name', parts[2]).first();
+      if (!serie) {
+        await db('series').insert({name: parts[2], season_id: season.id, telegram_file_id: video.video.file_id});
+        serie = await db('series').where('season_id', season.id).where('name', parts[2]).first();
+      }
+
+      await db('series').where('season_id', season.id).where('name', parts[2]).update({telegram_file_id: video.video.file_id});
+    });
 
     bot.on('text', async msg => {
+      if(msg.text.startsWith('/start')) {
+        //перед тем как запускать бота, нужно проверить , подписан ли чувак на канал , иначе ему предлагаем подписаться
+        
+        let subscribe = await bot.getChatMember(process.env.CHANNEL_CHAT_ID, msg.from.id);
+        
+        if (subscribe.status == 'left' || subscribe.status == 'kicked') {
 
-      await bot.sendMessage(msg.chat.id, `Второе меню`, {
+            await bot.sendMessage(msg.chat.id, `Чтобы начать пользоваться ботом, подпишись на канал <b>https://t.me/vksimpson</b> !`, {
+      
+                parse_mode: 'HTML'
+      
+            });
 
-        reply_markup: {
+            return;
+        }
 
-                inline_keyboard: [
+        //если это наш подписчик, то даем разрешение на авторизацию
+        let user = await db('users').where('telegram_id', msg.from.id).first();
 
-                    [{text: 'Стикер', callback_data: 'sticker'}, {text: 'Круглое Видео', callback_data: 'circleVideo'}],
-                    [{text: 'Купить Файл', callback_data: 'buyFile'}],
-                    [{text: 'Проверить Подписку', callback_data: 'checkSubs'}],
-                    [{text: 'Закрыть Меню', callback_data: 'closeMenu'}]
+        if (!user) {
+          await db('users').insert({'telegram_id': msg.from.id});
 
-                ]
+          user = await db('users').where('telegram_id', msg.from.id).first();
+        }
 
-            }
+        // тут подгружаем стартовое меню
+        showMenuMovies(msg);
 
-      })
-    
-    })
+        return;
+      }
+
+    });
 
     bot.on('callback_query', async ctx => {
-
         try {
-    
+            let args = ctx.data.split('-');
+            
 
-            switch(ctx.data) {
+            if (args[0] == 'selectMovie') {
+              // выбрали сериал
+              let id = args[1];
 
-              case "closeMenu":
-  
-                  await bot.deleteMessage(ctx.message.chat.id, ctx.message.message_id);
-                  break;
-  
-          }
-    
+              await db('users').where('telegram_id', ctx.from.id).update({select_movie_id: id});
+
+              await showMenuSeasons(ctx);
+
+              return;
+            }
+
+            if (args[0] == 'selectSeason') {
+              // выбрали сезон
+              let id = args[1];
+
+              await db('users').where('telegram_id', ctx.from.id).update({select_seasons_id: id});
+
+              await showMenuSeries(ctx);
+
+              return;
+            }
+
+            if (args[0] == 'selectSerie') {
+              // выбрали серию
+              let id = args[1];
+
+              await db('users').where('telegram_id', ctx.from.id).update({select_series_id: id});
+
+              await showVideo(ctx);
+
+              return;
+            }
+
+            if (args[0] == 'backToSeries') {
+              // возвращаемся в меню c сериями
+              await showMenuSeries(ctx);
+
+              return;
+            }
+
+            if (args[0] == 'backToSeasons') {
+              // возвращаемся в меню с сезонами
+              await showMenuSeasons(ctx);
+
+              return;
+            }
+
+            if (args[0] == 'backToMovies') {
+              // возвращаемся в меню с сериалами
+              await showMenuMovies(ctx, true);
+
+              return;
+            }
+
+            if (args[0] == 'closeVideo') {
+              bot.deleteMessage(ctx.message.chat.id, ctx.message.message_id);
+              return;
+            }
         }
         catch(error) {
-    
             console.log(error);
-    
         }
     
     })
+}
+
+async function showMenuMovies(msg, isMenu = false) {
+  let movies = await db('movies').where('has_active', 1);
+  let list = [];
+
+  movies.forEach(item => {
+    list.push([{text: item.name, callback_data: `selectMovie-${item.id}`}]);
+  });
+
+  let messageId = isMenu ? msg.message.message_id : msg.message_id;
+  let chatId = isMenu ? msg.message.chat.id : msg.chat.id;
+
+  await bot.deleteMessage(chatId, messageId);
+
+  await bot.sendMessage(chatId, `Выберите сериал`, {
+      reply_markup: {
+          inline_keyboard: list
+      },
+  });
+}
+
+async function showMenuSeasons(msg) {
+  let user = await db('users').where('telegram_id', msg.from.id).first();
+  let seasons = await db('seasons').where('has_active', 1).where('movie_id', user.select_movie_id);
+  let list = [];
+
+  seasons.forEach(item => {
+    list.push([{text: item.name, callback_data: `selectSeason-${item.id}`}]);
+  });
+
+  list.push([{text: 'Назад', callback_data: 'backToMovies'}])
+
+  await bot.deleteMessage(msg.message.chat.id, msg.message.message_id);
+
+  await bot.sendMessage(msg.message.chat.id, `Выберите сезон`, {
+      reply_markup: {
+          inline_keyboard: list
+      }
+  });
+}
+
+async function showMenuSeries(msg) {
+  let user = await db('users').where('telegram_id', msg.from.id).first();
+  let series = await db('series').where('has_active', 1).where('season_id', user.select_seasons_id);
+  let list = [];
+
+  series.forEach(item => {
+    list.push([{text: item.name, callback_data: `selectSerie-${item.id}`}]);
+  });
+
+  list.push([{text: 'Назад', callback_data: 'backToSeasons'}]);
+
+  await bot.deleteMessage(msg.message.chat.id, msg.message.message_id);
+
+  await bot.sendMessage(msg.message.chat.id, `Выберите серию`, {
+      reply_markup: {
+          inline_keyboard: list
+      },
+  });
+}
+
+async function showVideo(msg) {
+  let user = await db('users').where('telegram_id', msg.from.id).first();
+  let movie = await db('movies').where('has_active', 1).where('id', user.select_movie_id).first();
+  let season = await db('seasons').where('has_active', 1).where('id', user.select_seasons_id).first();
+  let serie = await db('series').where('has_active', 1).where('id', user.select_series_id).first();
+  
+  let list = [[{text: 'Закрыть', callback_data: 'closeVideo'}]];
+
+  await showMenuSeries(msg);
+
+  await bot.sendDocument(msg.message.chat.id, serie.telegram_file_id, {
+      reply_markup: {
+          inline_keyboard: list
+      },
+      caption: `${movie.name} | ${season.name} | ${serie.name}`
+  });
 }
 
 setTimeout(async () => await _main(), 300);
